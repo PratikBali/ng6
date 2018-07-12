0d49b163e2513dd229bb75d64d62e708124a4ae0
pratikbali <pratikbali96@gmail.com>
2018-07-12 20:46:27 +0530

    assignment 7

--- src/app/webapp/asgn7/book/book.component.ts
+++ src/app/webapp/asgn7/book/book.component.ts
@@ -10,20 +10,21 @@ export class BookComponent implements OnInit {
   ngOnInit() {
   }
 
-  displayedColumns: string[] = ['name', 'author', 'pages'];
+  displayedColumns: string[] = ['name', 'author', 'pages', 'price'];
   dataSource = bookData;
 
   export interface Books {
     name: string;
     author: string;
     pages: number;
+    price: number;
   }
 
   const bookData: Books[] = [
-    { name: 'Hydrogen', author: 'Pratik Bali', pages: 1000},
-    { name: 'C', author: 'Denis Ritchie', pages: 600},
-    { name: 'C++', author: 'Bjarne Straustrup', pages: 400},
-    { name: 'Java', author: 'James Gosling', pages: 800},
+    { name: 'Hydrogen', author: 'Pratik Bali', pages: 1000, price: 1000},
+    { name: 'C', author: 'Denis Ritchie', pages: 600, price: 1000},
+    { name: 'C++', author: 'Bjarne Straustrup', pages: 400, price: 1000},
+    { name: 'Java', author: 'James Gosling', pages: 800, price: 1000},
 
   ];
 
